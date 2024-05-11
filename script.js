84edbc88f3f747d391b90b470ece653504337910
VitorOliveira936 <vitoroliveiraangel936@gmail.com>
2024-05-11 10:01:26 -0300

    alterações de comparação js

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ function getValues() {
     const inputOne = Math.ceil(document.getElementById("number-one").value);
     const inputTwo = Math.floor(document.getElementById("random-number").value);
 
-    const drawer = Math.floor(Math.random() * (inputTwo - inputOne + 1)) + inputOne;
+    if (inputOne < inputTwo) {
+        const drawer = Math.floor(Math.random() * (inputTwo - inputOne + 1)) + inputOne;
 
-    alert(drawer);
+        alert(drawer);
+
+    } else if(inputOne >= inputTwo) {
+
+        alert("O Primeiro Número PRECISA ser menor que o primero");
+    }
+    
 
 }
 
